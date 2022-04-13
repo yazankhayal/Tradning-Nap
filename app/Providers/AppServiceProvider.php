0a3f69ad76ccc10dc8bac3ef25179941101d4818
Yazan Khayal <yazan.khayal@gmail.com>
2022-04-13 23:33:22 +0100

    Force HTTPS

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         //
+        if($this->app->environment('production')) {
+            \URL::forceScheme('https');
+        }
         Schema::defaultStringLength(191);
         view()->composer('*', function ($view) {
 
